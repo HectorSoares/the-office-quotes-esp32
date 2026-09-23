ded61a4180d3083bec20eb34470a2a45fa3e64ca
Hector Soares <hectorjpsoares@gmail.com>
2026-09-23 19:11:17 -0300

    feat: add second page to long quotes

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
-#include <Arduino.h>
-#include <SPI.h>
-#include <GxEPD2_3C.h>
-#include <Fonts/FreeMonoBold9pt7b.h>
 #include "the_office_bitmaps.h"
 #include "the_office_quotes.h"
+#include <Arduino.h>
+#include <Fonts/FreeMonoBold9pt7b.h>
+#include <GxEPD2_3C.h>
+#include <SPI.h>
 
 #define BUTTON_PIN 0
 
@@ -14,19 +14,29 @@
 #define EPD_RST 9
 #define EPD_BUSY 10
 
-GxEPD2_3C<GxEPD2_290_C90c, GxEPD2_290_C90c::HEIGHT> display(
-    GxEPD2_290_C90c(EPD_CS, EPD_DC, EPD_RST, EPD_BUSY));
+GxEPD2_3C<GxEPD2_290_C90c, GxEPD2_290_C90c::HEIGHT>
+    display(GxEPD2_290_C90c(EPD_CS, EPD_DC, EPD_RST, EPD_BUSY));
 
-struct Quote
-{
+const int TEXT_X = 138;
+const int TEXT_Y = 32;
+const int TEXT_W = 158;
+const int TEXT_H = 94;
+const int LINE_HEIGHT = 15;
+const int VISIBLE_LINES = 6;
+const int MAX_LINES = 20;
+
+struct Quote {
   String character;
   String text;
 };
 
+Quote currentQuote;
+String quoteLines[MAX_LINES];
+int totalLines = 0;
+int firstVisibleLine = 0;
 bool lastButtonState = HIGH;
 
-const uint8_t *getCharacterBitmap(const String &character)
-{
+const uint8_t *getCharacterBitmap(const String &character) {
   if (character == "Michael Scott")
     return epd_bitmap_michael;
   if (character == "Jim Halpert")
@@ -47,12 +57,10 @@ const uint8_t *getCharacterBitmap(const String &character)
     return epd_bitmap_kelvin;
   if (character == "Andy Bernard")
     return epd_bitmap_andy;
-
   return nullptr;
 }
 
-String cleanText(String text)
-{
+String cleanText(String text) {
   text.replace("\xE2\x80\x99", "'");
   text.replace("\xE2\x80\x98", "'");
   text.replace("\xE2\x80\x9C", "\"");
@@ -63,57 +71,109 @@ String cleanText(String text)
 
   String result;
 
-  for (size_t i = 0; i < text.length(); i++)
-  {
+  for (size_t i = 0; i < text.length(); i++) {
     uint8_t c = text[i];
-
     if (c >= 32 && c <= 126)
-    {
       result += (char)c;
-    }
   }
-
   return result;
 }
 
-Quote getRandomQuote()
-{
+Quote getRandomQuote() {
   Quote result;
 
   if (QUOTE_COUNT == 0)
-  {
     return result;
-  }
 
   size_t index = random(QUOTE_COUNT);
 
   result.character = quotes[index].character;
   result.text = cleanText(quotes[index].text);
-
   return result;
 }
 
-void showQuote(const Quote &quote)
-{
-  const uint8_t *bitmap = getCharacterBitmap(quote.character);
+void prepareQuoteLines() {
+  totalLines = 0;
+  firstVisibleLine = 0;
+
+  String textToWrap = "\"" + currentQuote.text + "\"";
+  String currentLine = "";
+
+  const int maxWidth = 296 - TEXT_X - 6;
+
+  while (textToWrap.length() > 0 && totalLines < MAX_LINES) {
+    int spaceIndex = textToWrap.indexOf(' ');
+
+    if (spaceIndex == -1)
+      spaceIndex = textToWrap.length();
+
+    String word = textToWrap.substring(0, spaceIndex);
+
+    String testLine;
+
+    if (currentLine.length() == 0) {
+      testLine = word;
+    } else {
+      testLine = currentLine + " " + word;
+    }
+
+    int16_t bx, by;
+    uint16_t bw, bh;
+
+    display.getTextBounds(testLine, TEXT_X, TEXT_Y, &bx, &by, &bw, &bh);
+
+    if (bw > maxWidth && currentLine.length() > 0) {
+      quoteLines[totalLines++] = currentLine;
+      currentLine = word;
+    } else {
+      currentLine = testLine;
+    }
+
+    if (spaceIndex == textToWrap.length()) {
+      textToWrap = "";
+    } else {
+      textToWrap = textToWrap.substring(spaceIndex + 1);
+    }
+  }
+
+  if (currentLine.length() > 0 && totalLines < MAX_LINES) {
+    quoteLines[totalLines++] = currentLine;
+  }
+}
+
+void drawQuoteLines() {
+  display.setFont(NULL);
+  display.setTextSize(1);
+  display.setTextColor(GxEPD_BLACK);
+
+  int cursorY = TEXT_Y + 8;
+
+  for (int i = 0; i < VISIBLE_LINES; i++) {
+    int lineIndex = firstVisibleLine + i;
+
+    if (lineIndex >= totalLines) {
+      break;
+    }
+
+    display.setCursor(TEXT_X, cursorY);
+    display.print(quoteLines[lineIndex]);
+
+    cursorY += LINE_HEIGHT;
+  }
+}
+
+void drawInitialScreen() {
+  const uint8_t *bitmap = getCharacterBitmap(currentQuote.character);
 
   display.setRotation(1);
-  display.setPartialWindow(0, 0, 296, 128);
+  display.setFullWindow();
   display.firstPage();
 
-  do
-  {
+  do {
     display.fillScreen(GxEPD_WHITE);
 
-    if (bitmap != nullptr)
-    {
-      display.drawInvertedBitmap(
-          0,
-          0,
-          bitmap,
-          128,
-          128,
-          GxEPD_BLACK);
+    if (bitmap != nullptr) {
+      display.drawInvertedBitmap(0, 0, bitmap, 128, 128, GxEPD_BLACK);
     }
 
     display.drawFastVLine(132, 0, 128, GxEPD_RED);
@@ -121,94 +181,29 @@ void showQuote(const Quote &quote)
     display.setFont(&FreeMonoBold9pt7b);
     display.setTextColor(GxEPD_RED);
     display.setCursor(142, 18);
-    display.print(quote.character);
+    display.print(currentQuote.character);
 
-    display.setFont(NULL);
-    display.setTextSize(1);
-    display.setTextColor(GxEPD_BLACK);
-
-    String textToPrint = "\"" + quote.text + "\"";
-    int cursorX = 142;
-    int cursorY = 40;
-    int maxWidth = 296 - 142 - 6;
-    int lineHeight = 13;
-
-    String currentLine = "";
-    int spaceIndex = 0;
-
-    while (textToPrint.length() > 0)
-    {
-      spaceIndex = textToPrint.indexOf(' ');
-
-      if (spaceIndex == -1)
-      {
-        spaceIndex = textToPrint.length();
-      }
-
-      String word = textToPrint.substring(0, spaceIndex);
-      String testLine = currentLine.length() == 0
-                            ? word
-                            : currentLine + " " + word;
-
-      int16_t bx, by;
-      uint16_t bw, bh;
-
-      display.getTextBounds(
-          testLine,
-          cursorX,
-          cursorY,
-          &bx,
-          &by,
-          &bw,
-          &bh);
-
-      if (bw > maxWidth && currentLine.length() > 0)
-      {
-        display.setCursor(cursorX, cursorY);
-        display.print(currentLine);
-
-        cursorY += lineHeight;
-        currentLine = word;
-
-        if (cursorY > 120)
-        {
-          display.setCursor(cursorX, cursorY - 2);
-          display.print("...");
-          break;
-        }
-      }
-      else
-      {
-        currentLine = testLine;
-      }
-
-      if (spaceIndex == textToPrint.length())
-      {
-        textToPrint = "";
-      }
-      else
-      {
-        textToPrint = textToPrint.substring(spaceIndex + 1);
-      }
-    }
-
-    if (currentLine.length() > 0 && cursorY <= 120)
-    {
-      display.setCursor(cursorX, cursorY);
-      display.print(currentLine);
-    }
+    drawQuoteLines();
 
   } while (display.nextPage());
 }
 
-void showError(const char *message)
-{
+void drawNextTextPage() {
+  display.setPartialWindow(TEXT_X, TEXT_Y, TEXT_W, TEXT_H);
+  display.firstPage();
+
+  do {
+    display.fillRect(TEXT_X, TEXT_Y, TEXT_W, TEXT_H, GxEPD_WHITE);
+    drawQuoteLines();
+  } while (display.nextPage());
+}
+
+void showError(const char *message) {
   display.setRotation(1);
   display.setFullWindow();
   display.firstPage();
 
-  do
-  {
+  do {
     display.fillScreen(GxEPD_WHITE);
 
     display.setFont(&FreeMonoBold9pt7b);
@@ -216,6 +211,7 @@ void showError(const char *message)
     display.setCursor(10, 30);
     display.print("The Office");
 
+    display.setFont(NULL);
     display.setTextColor(GxEPD_BLACK);
     display.setCursor(10, 60);
     display.print(message);
@@ -223,54 +219,73 @@ void showError(const char *message)
   } while (display.nextPage());
 }
 
-void loadQuote()
-{
-  Quote quote = getRandomQuote();
+void loadQuote() {
+  currentQuote = getRandomQuote();
+
+  if (currentQuote.character.length() == 0 || currentQuote.text.length() == 0) {
+    showError("No quotes available");
+    return;
+  }
 
   Serial.println();
   Serial.print("Character: ");
-  Serial.println(quote.character);
+  Serial.println(currentQuote.character);
 
   Serial.print("Quote: ");
-  Serial.println(quote.text);
+  Serial.println(currentQuote.text);
 
-  showQuote(quote);
+  prepareQuoteLines();
+
+  Serial.print("Total lines: ");
+  Serial.println(totalLines);
+
+  drawInitialScreen();
 }
 
-void setup()
-{
+void nextQuotePage() {
+  if (firstVisibleLine + VISIBLE_LINES < totalLines) {
+    firstVisibleLine += VISIBLE_LINES;
+
+    Serial.print("Showing lines ");
+    Serial.print(firstVisibleLine + 1);
+    Serial.print(" - ");
+
+    int lastLine = firstVisibleLine + VISIBLE_LINES;
+
+    if (lastLine > totalLines) {
+      lastLine = totalLines;
+    }
+
+    Serial.println(lastLine);
+
+    drawNextTextPage();
+  } else {
+    loadQuote();
+  }
+}
+
+void setup() {
   Serial.begin(115200);
   delay(500);
 
   pinMode(BUTTON_PIN, INPUT_PULLUP);
-
-  SPI.begin(
-      EPD_SCK,
-      -1,
-      EPD_MOSI,
-      EPD_CS);
-
+  SPI.begin(EPD_SCK, -1, EPD_MOSI, EPD_CS);
   display.init(115200);
-
   randomSeed(esp_random());
 
   loadQuote();
 }
 
-void loop()
-{
+void loop() {
   bool buttonState = digitalRead(BUTTON_PIN);
 
-  if (lastButtonState == HIGH && buttonState == LOW)
-  {
+  if (lastButtonState == HIGH && buttonState == LOW) {
     delay(30);
 
-    if (digitalRead(BUTTON_PIN) == LOW)
-    {
-      loadQuote();
+    if (digitalRead(BUTTON_PIN) == LOW) {
+      nextQuotePage();
 
-      while (digitalRead(BUTTON_PIN) == LOW)
-      {
+      while (digitalRead(BUTTON_PIN) == LOW) {
         delay(10);
       }
     }
